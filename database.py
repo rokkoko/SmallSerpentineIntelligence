48e55afb44007c66b78e0b4a6fef21bddbf61568
egor187 <py.egor.py@gmail.com>
2021-01-06 18:09:54 +0300

    Rename functions to a 'present_time' syntax; fix indents

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import psycopg2
 import parse_message
 from flask import request
 import datetime as date
+
 """
 Данные для подключения берем из .env файла, чтобы не хранить их в открытом виде в коде. https://docs.repl.it/repls/secret-keys
 """
@@ -19,32 +20,32 @@ cur.execute("SELECT score, game_session_id FROM scores;")
 
 print(cur.fetchall())
 
-#---------------------------------------------
+# ---------------------------------------------
 # {"value": "Червяки: Егор 1, Саша 5, Сергей 0"}
 # parse.parseMessage()  - [0] - game_name, [1] - dict_with_stats
 
 game_name = parse_message.parse(request.form['value'])[0]
 stats = parse_message.parse(request.form['value'])[1]
 
+
 # Для добавление строки в таблицы gamers и games необходимо проверить является ли игра/игрок новыми для БД. Для этого используем "множества"
 
 # https://stackoverflow.com/questions/5247685/python-postgres-psycopg2-getting-id-of-row-just-inserted
 
 
+def add_game_into_db(game):
+    """Adding game to DB and return it's id from table. If game already in db - pass this step and print error message"""
+    try:
+        cur.execute("INSERT INTO games (game_name) VALUES (%(game)s);", {'game': game})
+        conn.commit()
+    except:
+        print('game already in DB')
+        conn.commit()
+    cur.execute("SELECT id FROM games WHERE game_name = (%(game)s);", {'game': game})
+    return cur.fetchone()[0]
 
-def adding_game_into_db(game):
-  """Adding game to DB and return it's id from table. If game already in db - pass this step and print error message"""
-  try:
-    cur.execute("INSERT INTO games (game_name) VALUES (%(game)s);", {'game': game})
-    conn.commit()
-  except:
-    print('game already in DB')
-    conn.commit()
-  cur.execute("SELECT id FROM games WHERE game_name = (%(game)s);", {'game': game})
-  return cur.fetchone()[0]
 
-
-def adding_users_into_db(users: dict):
+def add_users_into_db(users: dict):
     """
     Adding seq of users to DB and return it's id from table. If user already in db - pass this step and print error
     message.
@@ -61,7 +62,7 @@ def adding_users_into_db(users: dict):
     return cur.fetchall()
 
 
-def adding_game_session_into_db(game_id):
+def add_game_session_into_db(game_id):
     """
     Adding game_session to DB and return it's id from table
     :param game_id: from func adding_game_into_db() from func parseMessage()
