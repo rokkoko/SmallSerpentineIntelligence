735f7c128a36474db2f8cfcd04fb6f50d7ad4fcb
Alexander Doroshenko <alexander.doroshenko@getpenta.com>
2021-01-07 07:42:42 +0100

    Update test to support current `parse_message` contract

--- test_parse_message.py
+++ test_parse_message.py
@@ -1,25 +1,26 @@
 import pytest
+
 from parse_message import parse_message
 
 
 def test_parse_message_of_correct_text():
     """Test happy path"""
-    text = "Червяки: Егор 1, Саша 5, Сергей 0, Юля 3"
+    incoming_message = {"value": "Червяки: Егор 1, Саша 5, Сергей 0, Юля 3"}
     expected_result = ("Червяки", ("Егор", 1), ("Саша", 5), ("Сергей", 0), ("Юля", 3))
-    assert parse_message(text) == expected_result
+    assert parse_message(incoming_message) == expected_result
 
 
 text_no_colon = "Червяки Егор 1, Саша 5, Сергей 0, Юля 3"
 text_no_comma = "Червяки: Егор 1 Саша 5, Сергей 0, Юля 3"
 text_no_score = "Червяки: Егор 1, Саша, Сергей 0, Юля 3"
 test_data = [
-    (text_no_colon, ValueError),
-    (text_no_comma, ValueError),
-    (text_no_score, ValueError)
+    ({"value": text_no_colon}, ValueError),
+    ({"value": text_no_comma}, ValueError),
+    ({"value": text_no_score}, ValueError)
 ]
 
 
 @pytest.mark.parametrize("text,expected", test_data)
 def test_incorrect_text(text, expected):
-    """Test message we cant parse"""
+    """Test message we can't parse"""
     assert text == expected
