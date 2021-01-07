6b2f134018e463d66658ea7aee22c76c727f9a6a
Alexander Doroshenko <alexander.doroshenko@getpenta.com>
2021-01-07 11:08:30 +0100

    Remove testrunner, we will use PyCharm for now

--- testrunner.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from parse_message import parse
-
-def test_parse_message():
-  expected_message = 'Червяки: Егор 1, Саша 5, Сергей 0, Юля 3'
-  assert parse(expected_message) == ('Червяки', {'Егор': 1, 'Саша': 5, 'Сергей': 0, 'Юля': 3})
