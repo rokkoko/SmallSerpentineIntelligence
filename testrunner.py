7981c95037897cea64bc73c4961be4a27ca19ab7
egor187 <py.egor.py@gmail.com>
2021-01-08 15:32:10 +0300

    syntax fix

--- testrunner.py
+++ testrunner.py
@@ -1,6 +1,7 @@
 import pytest
 from parse_message import parse
 
+
 def test_parse_message():
-  expected_message = 'Червяки: Егор 1, Саша 5, Сергей 0, Юля 3'
-  assert parse(expected_message) == ('Червяки', {'Егор': 1, 'Саша': 5, 'Сергей': 0, 'Юля': 3})
+    expected_message = 'Червяки: Егор 1, Саша 5, Сергей 0, Юля 3'
+    assert parse(expected_message) == ('Червяки', {'Егор': 1, 'Саша': 5, 'Сергей': 0, 'Юля': 3})
