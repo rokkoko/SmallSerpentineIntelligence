5047fcdf31f52f810e695a04c7119c63d389ac46
Alexander Doroshenko <alexander.doroshenko@getpenta.com>
2021-02-03 19:28:35 +0100

    Add bot processor

--- parse_message.py
+++ parse_message.py
@@ -1,10 +1,9 @@
-def parse_message(incoming_message):
+def parse_message(message):
     """
-    :param incoming_message: from IFTTT
+    :param message: from IFTTT
     :return: tuple of 2 element: [0] game_name(str) and [1] score_pairs(dict)
     """
 
-    message = incoming_message['value']
     if ':' not in message:
         return message
     message = message.split(':')
