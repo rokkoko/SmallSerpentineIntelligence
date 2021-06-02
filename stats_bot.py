e53557591aeceb9b9a52f92fbfc9af8cbfd675c5
egor187 <py.egor.py@gmail.com>
2021-06-03 00:27:55 +0300

    testing bot 3

--- stats_bot.py
+++ stats_bot.py
@@ -24,16 +24,16 @@ class StatsBot:
     def __init__(self, token):
         self.bot = Bot(token)
         self.dispatcher = Dispatcher(self.bot, None, workers=0)
-        self.dispatcher.add_handler(CommandHandler("add", add_stats_command))
-        self.dispatcher.add_handler(CommandHandler("show", show_stats_command))
-        self.dispatcher.add_handler(
-            MessageHandler(activity_scores_message_filter & ~Filters.command, process_add_stats_message))
-        self.dispatcher.add_handler(
-            MessageHandler(known_activity_message_filter & ~activity_scores_message_filter & ~Filters.command,
-                           process_show_stats_message))
-        self.dispatcher.add_handler(
-            MessageHandler(~known_activity_message_filter & ~activity_scores_message_filter & ~Filters.command & ~Filters.animation & ~Filters.text,
-                           process_unknown_message))
+        # self.dispatcher.add_handler(CommandHandler("add", add_stats_command))
+        # self.dispatcher.add_handler(CommandHandler("show", show_stats_command))
+        # self.dispatcher.add_handler(
+        #     MessageHandler(activity_scores_message_filter & ~Filters.command, process_add_stats_message))
+        # self.dispatcher.add_handler(
+        #     MessageHandler(known_activity_message_filter & ~activity_scores_message_filter & ~Filters.command,
+        #                    process_show_stats_message))
+        # self.dispatcher.add_handler(
+        #     MessageHandler(~known_activity_message_filter & ~activity_scores_message_filter & ~Filters.command & ~Filters.animation & ~Filters.text,
+        #                    process_unknown_message))
 
 
         self.dispatcher.add_handler(
@@ -44,7 +44,7 @@ class StatsBot:
 
         self.dispatcher.add_handler(
             MessageHandler(
-                Filters.text & (~activity_scores_message_filter) & (~known_activity_message_filter) & (~Filters.command), test
+                Filters.text & (~Filters.command), test
             )
         )
 
